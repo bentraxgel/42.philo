09e917da126a57ff746e0c6a1a973d79e60a8535
bentraxgel <dhrtpdnjs@gmail.com>
2023-08-18 11:27:02 +0900

    2023년 8월 18일 금요일 11시 27분 02초 KST

--- philo/main.c
+++ philo/main.c
@@ -6,40 +6,50 @@
 /*   By: kumamon <kumamon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 21:22:44 by seok              #+#    #+#             */
-/*   Updated: 2023/08/16 11:40:30 by kumamon          ###   ########.fr       */
+/*   Updated: 2023/08/18 02:00:16 by kumamon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	arg_exception(int ac, char *av[], t_src *src)
+int	init_args(int ac, char *av[], t_arg *arg)
 {
 	int		i;
 
 	if (ac < 5 || ac > 6)
 		return (false);
-	src->total_philo = my_atoi(av[1]);
-	src->time_to_die = my_atoi(av[2]);
-	src->time_to_eat = my_atoi(av[3]);
-	src->time_to_sleep = my_atoi(av[4]);
+	arg->total_philo = my_atoi(av[1]);
+	arg->time_to_die = my_atoi(av[2]);
+	arg->time_to_eat = my_atoi(av[3]);
+	arg->time_to_sleep = my_atoi(av[4]);
 	if (av[5] != NULL)
-		src->must_eat = my_atoi(av[5]);
-	if (src->total_philo <= 0 || src->time_to_die <= 0 || \
-		src->time_to_eat <= 0 || src->time_to_sleep <= 0 || src->must_eat < 0)
+		arg->must_eat = my_atoi(av[5]);
+	if (arg->total_philo <= 0 || arg->time_to_die <= 0 || \
+		arg->time_to_eat <= 0 || arg->time_to_sleep <= 0 || arg->must_eat < 0)
 		return (false);
-	printf("%d, ", src->total_philo);
-	printf("%d, ", src->time_to_die);
-	printf("%d, ", src->time_to_eat);
-	printf("%d, ", src->time_to_sleep);
-	printf("%d\n", src->must_eat);
+	printf("%d, ", arg->total_philo);
+	printf("%d, ", arg->time_to_die);
+	printf("%d, ", arg->time_to_eat);
+	printf("%d, ", arg->time_to_sleep);
+	printf("%d\n", arg->must_eat);
 	return (true);
 }
 
 int	main(int ac, char *av[])
 {
-	t_src	src;
+	t_arg	arg;
 
-	if (arg_exception(ac, av, &src) == false)
+	if (init_args(ac, av, &arg) == false)
 		return (E_ARG);
 	if ()
+	// 철학자 한명일 경우 예외처리 따로 해야함.
+	// if (arg.total_philo == 1)
+	// 	return (0);
+	init_philo;
+}
+
+void	init_philo()
+{
+	pthread_t	philo;
+	
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: kumamon <kumamon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 21:27:34 by seok              #+#    #+#             */
-/*   Updated: 2023/08/16 21:11:41 by kumamon          ###   ########.fr       */
+/*   Updated: 2023/08/18 02:00:16 by kumamon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PHILO_H
 
 # include <pthread.h>
+# include <sys/time.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <stdbool.h>
@@ -29,19 +30,46 @@ typedef enum e_status
 	THINK
 }t_status;
 
-typedef struct s_src
+typedef struct s_arg
 {
-	int	num_of_philo;
+	int	total_philo;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
 	int	must_eat;
-}t_src;
+}t_arg;
+
+typedef	struct s_data
+{
+	double	start_time; //TODO 자료형뭐?
+	// 다먹은 시간??
+	// 포크?
+}t_data;
+/*
+쓰레드마다 pc레지스터를 독립할당한다
+- pc 레지스터 : 스레드가 명령어의 어디까지 수행했는지 나타냄.
+스레드가 어느부분까지 수행했느지 기억하기 위해 pc레지스터를 독립할당해야함.
+->TODO 철학자가 뭐했는지에 대해 표시할 필요있음.
+*/
 
 typedef struct s_philo
 {
-	
-};
+	pthread_t	philo_id;
+	int			philo_num;
+	int			eat_cnt;
+	long long	last_eat_time;
+	t_arg		arg;
+}t_philo;
+
+typedef	struct s_monitor
+{
+	pthread_mutex_t	print;
+	pthread_mutex_t	fork;
+	int				must_eat_flag;
+	int				dead_flag;
+
+}t_monitor;
+
 
 // library.c
 // int	my_atoi(const char *str, int *ret);
@@ -50,6 +78,6 @@ int	my_check_digit(const char *str);
 size_t	ft_strlen(const char *s);
 
 // main.c
-int	arg_exception(int ac, char *av[], t_src *src);
+int	init_args(int ac, char *av[], t_arg *arg);
 
 #endif
